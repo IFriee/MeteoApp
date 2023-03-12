8c87b58705b95ecd34a6abad2a0fe10cb525db73
IFriee <51403658+IFriee@users.noreply.github.com>
2023-03-12 20:00:13 +0100

    V1.6

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,38 +1,40 @@
-﻿namespace MeteoApp;
-
-public partial class MainPage : ContentPage
+﻿namespace MeteoApp
 {
-	RestService _restService;
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
 
-	public MainPage()
-	{
-		InitializeComponent();
-		_restService = new RestService();
-	}
+        // Constructeur de la classe MainPage
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService(); // Création d'une instance de RestService
+        }
 
+        // Méthode appelée lorsque l'utilisateur clique sur le bouton "Afficher"
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            // Vérifie si la ville a été saisie
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                // Appel de l'API pour obtenir les données de météo
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint));
 
-	async void OnGetWeatherButtonClicked(object sender, EventArgs e)
-	{
-		if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
-		{
-			WeatherData weatherData = await 
-				_restService.
-				GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint));
+                // Met à jour le contexte de liaison de données
+                BindingContext = weatherData;
+            }
+        }
 
-			BindingContext = weatherData;
+        // Génère l'URL de la requête pour l'API OpenWeatherMap
+        string GenerateRequestURL(string endPoint)
+        {
+            string requestUri = endPoint;
+            requestUri += $"?q={_cityEntry.Text}"; // Ajoute la ville à la requête
+            requestUri += "&units=metric"; // Définit l'unité de mesure en Celsius
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}"; // Ajoute la clé API
 
-		}
-	}
-
-	string GenerateRequestURL(string endPoint) {
-		string requestUri = endPoint;
-		requestUri += $"?q={_cityEntry.Text}";
-		requestUri += "&units=metric";
-		requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
-
-		return requestUri;
-	}
+            return requestUri;
+        }
+    }
 }
-
-
 
