b77ee5635acd9ab7119fe0f0b9d9784249ad023a
IFriee <51403658+IFriee@users.noreply.github.com>
2023-03-12 14:32:53 +0100

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "7644931008d77c7f5c8fd01185d87427";
+    }
+}
---
+++ RestService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoApp
+{
+    public class RestService
+    {
+        HttpClient client;
+       //constructeur de la classe RestService 
+        public RestService()
+        {
+            client = new HttpClient();
+        }
+        // envoit requete HTTP à un serv (https://openweathermap.org/) Si true, deserialise en objet WheatherData avec Nugget Newton (Json)
+        //Sinon, return Null
+        public async Task<WeatherData> GetWeatherData(string query){
+            WeatherData weatherData = null;
+
+            try
+            {
+                var response = await client.GetAsync(query);
+                if (response.IsSuccessStatusCode) {
+                    var content = await response.Content.ReadAsStringAsync();
+                    weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            return weatherData;
+
+        }
+    }
+}
