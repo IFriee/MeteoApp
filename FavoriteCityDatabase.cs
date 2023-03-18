46f8af254be0b7869f2f8dc18c06f54b148377e2
IFriee <51403658+IFriee@users.noreply.github.com>
2023-03-18 10:20:07 +0100

    Update FavoriteCityDatabase.cs

--- FavoriteCityDatabase.cs
+++ FavoriteCityDatabase.cs
@@ -9,17 +9,22 @@ namespace MeteoApp
     {
         readonly SQLiteAsyncConnection _database;
 
+        // Constructeur de la classe
         public FavoriteCityDatabase(string dbPath)
         {
             _database = new SQLiteAsyncConnection(dbPath);
+
+            // Créer la table FavoriteCity si elle n'existe pas déjà
             _database.CreateTableAsync<FavoriteCity>().Wait();
         }
 
+        // Récupère toutes les villes préférées stockées dans la base de données
         public Task<List<FavoriteCity>> GetFavoriteCitiesAsync()
         {
             return _database.Table<FavoriteCity>().ToListAsync();
         }
 
+        // Récupère une ville préférée spécifique à partir de son ID
         public Task<FavoriteCity> GetFavoriteCityAsync(int id)
         {
             return _database.Table<FavoriteCity>()
@@ -27,18 +32,22 @@ namespace MeteoApp
                             .FirstOrDefaultAsync();
         }
 
+        // Enregistre une ville préférée dans la base de données
         public Task<int> SaveFavoriteCityAsync(FavoriteCity favoriteCity)
         {
             if (favoriteCity.ID != 0)
             {
+                // Mettre à jour une ville préférée existante
                 return _database.UpdateAsync(favoriteCity);
             }
             else
             {
+                // Ajouter une nouvelle ville préférée
                 return _database.InsertAsync(favoriteCity);
             }
         }
 
+        // Supprime une ville préférée de la base de données
         public Task<int> DeleteFavoriteCityAsync(FavoriteCity favoriteCity)
         {
             return _database.DeleteAsync(favoriteCity);
